1863b81016afa29056a4d95150c1afbfef8648cb
Bilal Ahmed Khan <bilal.ahmed98@gmail.com>
2022-11-19 00:37:29 +0530

    ADDED: Snake and Ladder game played at start position 0

---
+++ SnakeLadder.java
@@ -0,0 +1,8 @@
+public class SnakeLadder {
+    public static void main(String[] args) {
+        int player = 1;
+        int position = 0;
+
+        System.out.println("Player in the position = " + position);
+    }
+}
