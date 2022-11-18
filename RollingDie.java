c8ab3964aee6379a1a991b0062c859f17f929824
Bilal Ahmed Khan <bilal.ahmed98@gmail.com>
2022-11-19 01:06:44 +0530

    ADDED: Code to check for option

--- RollingDie.java
+++ RollingDie.java
@@ -1,20 +1,37 @@
-import java.util.Scanner;
-
 public class RollingDie {
     static void rollTheDie() {
         int min = 1;
         int max = 6;
-        int Number = (int) Math.floor(Math.random() * (max - min) + min);
-        System.out.println("Die = " + Number);
-    }
+        int dieNumber = (int) Math.floor(Math.random() * (max - min) + min);
 
+        System.out.println("Die = " + dieNumber);
+    }
+    static void checkPlayer(int check) {
+
+        // check They are no play, Ladder or Snake
+        switch (check) {
+            case 0:
+                System.out.println("They are No play");
+                break;
+            case 1:
+                System.out.println("Ladder");
+                rollTheDie();
+                break;
+            case 2:
+                System.out.println("Snake");
+                rollTheDie();
+                break;
+            default:
+                System.out.println("Please try again");
+        }
+    }
     public static void main(String[] args) {
         int position = 0;
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter How many players want to play");
-        int player = sc.nextInt();
         System.out.println("Player in the position = " + position);
+        // Generate random Numbers
+        int check = (int) Math.floor((Math.random() * 10) % 3);
         // Function call
-        rollTheDie();
+        checkPlayer(check);
+
     }
 }
